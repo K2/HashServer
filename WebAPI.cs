682c7fa46da97a30d409747e96ff990a0af3baf2
K2 <ktwo@ktwo.ca>
2017-09-23 17:18:31 -0700

    Lots of fixes to the interactions between a local HashServer and the Internet hosted databases.  The local database is checked first then the request can be proxied (see config) to the internet hosted database if it's not found.

---
+++ WebAPI.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashServer
+{
+    public class WebAPI
+    {
+        static readonly HttpClient client;
+
+        static WebAPI()
+        {
+            client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            client.MaxResponseContentBufferSize = 1024 * 1024;
+            client.BaseAddress = new Uri(Program.Settings.External.gRoot);
+        }
+
+        public async static Task<HttpResponseMessage> POST(string data, string uri = "https://pdb2json.azurewebsites.net/api/PageHash/x")
+        {
+            var response = await client.PostAsync(uri, new StringContent(data, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+
+        public async static Task ProxyObject(string data, Stream writeTo, string uri = "https://pdb2json.azurewebsites.net/api/PageHash/x")
+        {
+            var response = await client.PostAsync(uri, new StringContent(data, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+            await response.Content.CopyToAsync(writeTo);
+        }
+    }
+}
